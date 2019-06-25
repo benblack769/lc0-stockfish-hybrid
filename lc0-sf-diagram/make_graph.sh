eb62ec183aa2c9b2adbdf8300c72eabbccbc2282
ben black <weepingwillowben@gmail.com>
2019-06-24 21:50:51 -0600

    added new stuff

--- lc0-sf-diagram/make_graph.sh
+++ lc0-sf-diagram/make_graph.sh
@@ -4,5 +4,7 @@ outdir=$3
 
 python make_diagram.py $cutoff  $indir/main.json | dot -Tsvg -o $outdir/main.svg
 python make_diagram.py $cutoff  $indir/middle.json | dot -Tsvg -o $outdir/middle.svg
+python make_diagram.py $cutoff  $indir/tactical.json | dot -Tsvg -o $outdir/tactical.svg
 python make_diagram.py $cutoff  $indir/nosf_main.json | dot -Tsvg -o $outdir/nosf_main.svg
 python make_diagram.py $cutoff  $indir/nosf_middle.json | dot -Tsvg -o $outdir/nosf_middle.svg
+python make_diagram.py $cutoff  $indir/nosf_tactical.json | dot -Tsvg -o $outdir/nosf_tactical.svg
